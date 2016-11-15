552c9dc52035c3d9b281d810b0fe3c7b5c0c3ad4
Koichi Ishida <wapa5pow@gmail.com>
2016-11-15 16:50:11 +0900

    add rack-action_logger to rails4

--- lib/rack/action_logger/controller_concerns/request_log.rb
+++ lib/rack/action_logger/controller_concerns/request_log.rb
@@ -10,7 +10,9 @@ module Rack::ActionLogger::ControllerConcerns
 
     def set_request_log
       Rack::ActionLogger::Container.set_request_log({ path_info: request.path_info, request_method: request.request_method }, 'action.request')
-      Rack::ActionLogger::Container.merge_attributes({ request_id: request.request_id })
+
+      request_id = Rails::VERSION::MAJOR >= 5 ? request.request_id : request.uuid
+      Rack::ActionLogger::Container.merge_attributes({ request_id: request_id })
     end
   end
 end
