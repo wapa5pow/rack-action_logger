9a7f540433092c99be9fc5d8c2e5d312463b0ac8
Koichi Ishida <wapa5pow@gmail.com>
2016-11-15 17:22:49 +0900

    remove unnecessary autoload

--- lib/rack/action_logger/emit_adapter.rb
+++
@@ -1,9 +0,0 @@
-module Rack::ActionLogger
-  module EmitAdapter
-    extend ActiveSupport::Autoload
-
-    autoload :FluentAdapter
-    autoload :LoggerAdapter
-    autoload :NullAdapter
-  end
-end
