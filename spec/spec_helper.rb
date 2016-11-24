f4ab36eae2492ec142c1a0c3cdcd1082f8340b7a
Koichi Ishida <wapa5pow@gmail.com>
2016-11-24 17:13:53 +0900

    update simplecov settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 require 'rspec'
 require 'simplecov'
 require 'codeclimate-test-reporter'
+
+dir = File.join('coverage')
+SimpleCov.coverage_dir(dir)
+
+SimpleCov.start do
+  add_filter '/vendor/'
+  add_filter '/spec/'
+  add_filter '/example/'
+  add_filter '/docs/'
+
+  add_group 'lib', 'lib'
+end
+
 require 'rack/action_logger'
 require 'rack/action_logger/emit_adapter/null_adapter'
 require 'helper/test_application_helper'
